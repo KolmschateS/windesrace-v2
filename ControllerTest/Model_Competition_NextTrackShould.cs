700a19eb15c7312b5106763d795a586a9c570b32
Sebastiaan Colmen <skolmschate@hotmail.nl>
2021-09-08 13:29:45 +0200

    3.1 done

---
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using Model;
+using NUnit.Framework;
+
+namespace ControllerTest
+{
+    [TestFixture]
+    public class Model_Competition_NextTrackShould
+    {
+        public Competition Competition;
+        [SetUp]
+        public void SetUp()
+        {
+            Competition = new Competition();
+        }
+
+        [Test]
+        public void NextTrack_EmptyQueue_ReturnNull()
+        {
+            var result = Competition.NextTrack();
+            Assert.IsNull(result);
+        }
+    }
+}
