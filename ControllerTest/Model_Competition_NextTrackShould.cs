3b33f4b3c5b147bed0ceb128d40c1ee3943615b3
Sebastiaan Colmen <skolmschate@hotmail.nl>
2021-09-08 14:51:19 +0200

    3.2 done

--- ControllerTest/Model_Competition_NextTrackShould.cs
+++ ControllerTest/Model_Competition_NextTrackShould.cs
@@ -20,5 +20,14 @@ namespace ControllerTest
             var result = Competition.NextTrack();
             Assert.IsNull(result);
         }
+        
+        [Test]
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            Track testTrack = new Track("testTrack", new SectionTypes[]{SectionTypes.Finish});
+            Competition.Tracks.Enqueue(testTrack);
+            var result = Competition.NextTrack();
+            Assert.AreEqual(testTrack, result);
+        }
     }
 }
